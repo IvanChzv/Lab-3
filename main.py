ed4fbd37e146ed058b4eae9edb1bd77fd11f6d2f
IvanChzv <125931001+IvanChzv@users.noreply.github.com>
2024-03-11 00:21:10 +0300

    Update

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ def index():
     current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return render_template('index.html', current_datetime=current_datetime)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='localhost', port=8080)

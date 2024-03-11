d29eafebb97dab4ff37a4e95ce508cc06ac3e51f
IvanChzv <125931001+IvanChzv@users.noreply.github.com>
2024-03-11 22:25:22 +0300

    Update main.py and index.html

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 app = Flask(__name__)
 @app.route('/')
 def index():
-    current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return render_template('index.html', current_datetime=current_datetime)
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    return render_template('index.html', current_date=current_date)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
